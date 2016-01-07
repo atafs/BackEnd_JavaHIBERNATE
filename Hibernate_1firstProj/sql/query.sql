b690fe9fd613705ac81434b2985a3115e78c4329
atafs <atafs@iscte-iul.pt>
2016-01-07 12:52:23 +0000

    update

--- Hibernate_1firstProj/sql/query.sql
+++ Hibernate_1firstProj/sql/query.sql
@@ -1,3 +1,19 @@
 ﻿--QUERY: CHECK TABLE FROM 
-SELECT * FROM user_details 
+SELECT *
+FROM user_details 
+;
+
+--QUERY: CHECK TABLE FROM 
+SELECT *
+FROM user_address
+;
+
+--QUERY: CHECK TABLE FROM 
+SELECT *
+FROM hibernate_unique_key
+;
+
+--QUERY: CHECK TABLE FROM 
+SELECT user_id, user_name, joineddate, description, address
+FROM user_details 
 ;
